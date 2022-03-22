3a9bb93e7727a1cc4b3f5bd0a5852c26cbb1451e
AbdulahiDayur <Abdulahidayur@gmail.com>
2022-03-22 12:33:04 -0700

    showing & hiding form modal

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-// async function getAPI() {
-//   const apiURL = ''
-//   try {
-//     const res = await axios.get(apiURL);
-//     const data = await res.data;
+const modalContainer = document.getElementById("modal");
+const modalShow = document.getElementById("show-modal");
+const modalClose = document.getElementById("close-modal");
+const bookmarkForm = document.getElementById("bookmark-form");
+const websiteNameEl = document.getElementById("website-name");
+const websiteUrlEl = document.getElementById("website-url");
+const bookmarksContainer = document.getElementById("bookmarks-container");
 
-//   } catch (error) {
-//     console.log('Whoops', error);
-//   }
-// }
+// Show Modal
+function showModal() {
+  modalContainer.classList.add('show-modal');
+  websiteNameEl.focus();
+}
+
+// Modal Event Listeners
+modalShow.addEventListener('click', showModal);
+modalClose.addEventListener("click", () => modalContainer.classList.remove('show-modal'));
+
+window.addEventListener('click', (e) => {
+  e.target == modalContainer ? modalContainer.classList.remove('show-modal') : false
+});
+
