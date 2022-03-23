a28cfdb70dac9b036b07944b8aa6a6e969fc54f0
AbdulahiDayur <Abdulahidayur@gmail.com>
2022-03-22 17:42:50 -0700

    implemented form validation

--- script.js
+++ script.js
@@ -19,7 +19,20 @@ window.addEventListener('click', (e) => {
   e.target == modalContainer ? modalContainer.classList.remove('show-modal') : false
 });
 
-
+// Validate Form 
+function validate(nameValue, urlValue) {
+  const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+  const regex = new RegExp(expression);
+  if(!urlValue || !nameValue) {
+    alert('Please submit values for both fields.');
+    return false
+  }
+  if(!urlValue.match(regex)) {
+    alert('Please provide a valid web address');
+    return false;
+  }
+  return true;
+}
 
 // Handle Data from form
 function storeBookmark(e) {
@@ -29,7 +42,11 @@ function storeBookmark(e) {
   if (!urlValue.includes('https://') && !urlValue.includes('http://')) {
     urlValue = `https://${urlValue}`;
   }
-  console.log(nameValue, urlValue);
+  
+  if (!validate(nameValue, urlValue)){
+    return false;
+  }
+  
 }
 
 // Event Listerner
